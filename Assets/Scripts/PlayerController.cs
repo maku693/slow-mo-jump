aeb0742765d87e05457e3ab3fd1b617e6fd0f3d2
Shota HAMADA <maku3ts@gmail.com>
2017-10-06 23:53:38 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public GameObject floor;
+    private Rigidbody rb;
+    private bool collidingToFloor = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == floor)
+        {
+            collidingToFloor = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collidingToFloor = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (collidingToFloor)
+            {
+                rb.AddForce(0.0f, rb.mass * 1000.0f, 0.0f);
+            }
+        }
+    }
+}
